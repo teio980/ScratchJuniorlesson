c1f2f0c9dff271ae81c59279c82d423b34685c67
LiawYongLoon <yongloon0927@gamil.com>
2025-02-28 00:35:32 +0800

    Use Regular Expression to valid user password

--- register.js
+++ register.js
@@ -1,13 +1,20 @@
 function checkPassword(){
     let password = document.getElementById("U_Password").value;
     let C_password = document.getElementById("U_Confirmed_Password").value;
+    const condition = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d).{6,}$/;
 
     if (password !== C_password) {
         alert("Password do not match!");
         location.reload();
         return false;
     } else {
-        
+        if (condition.test(password)) {
+            console.log("密码有效！");
+        } else {
+            console.log("密码不符合要求！");
+        }
         return true;
     }
+
 }
+
