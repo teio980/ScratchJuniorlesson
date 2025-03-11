9ab276d1fe6e1b036ebea37bbc2144c8d58c46ff
LiawYongLoon <yongloon0927@gamil.com>
2025-03-11 22:34:53 +0800

    Done Header And Contact Us Page

--- login.php
+++ login.php
@@ -4,9 +4,26 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="cssfile/login.css">
+    <link rel="stylesheet" href="cssfile/header.css">
     <title>Document</title>
 </head>
 <body>
+    <div class="main_header">
+        <div class="leftsection">
+            <div class="logo"><img src="" alt=""></div>
+            <div class="menu_choice">
+                <a href="landingpage.html">Home</a>
+                <a href="AboutUs.html">About Us</a>
+                <a href="Course.html">Course</a>
+                <a href="contactUs.html">Contact Us</a>
+            </div>
+        </div>
+        <div class="rightsection">
+            <a href="login.php">Sign In</a>
+            <a href="register.html">Create Account</a>
+            <a href="frequencyAskQuestions.html">FAQ</a>
+        </div>
+    </div>
     <div class="main">
         <div class="left"><img src="" alt=""></div>
         <div class="right" >
